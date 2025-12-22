cf7fe354697329d7f6d4a0ac29fc4ff45c26b498
swayam50 <swayamsidh50@gmail.com>
2025-12-22 18:42:35 +0530

    updated folder structure

--- spring-security/spring-security-default/src/main/java/io/wulfcodes/secc/config/SecurityConfig.java
+++ spring-security/spring-security-default/src/main/java/io/wulfcodes/secc/config/SecurityConfig.java
@@ -1,9 +1,78 @@
 package io.wulfcodes.secc.config;
 
+import java.util.Objects;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    /** SecurityFilterChain used -> org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration.SecurityFilterChainConfiguration
+        - It enables FormLogin (using FormLoginConfigurer) and Basic Auth (using HttpBasicConfigurer) by default and authenticates all the requests
+        -
+        - Uses DaoAuthenticationProvider by default for validation
+        - Uses InMemoryUserDetailsManager with a single user with username "user" and a randomly generated password by default for UserDetailsService
+
+        - Creating custom bean of any component, such SecurityFilter, AuthenticationProvider, UserDetailsService, PasswordEncoder
+          will override the default configuration of those classes
+     */
+
+    @Bean
+    public AuthenticationProvider customAuthenticationProvider(UserDetailsService userDetailsService) {
+        return new AuthenticationProvider() {
+
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                System.out.println("Custom Authentication Provider");
+                String username = (String)authentication.getPrincipal();
+                String password = (String)authentication.getCredentials();
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username); // Will throw UserNotFoundException -> AuthenticationException when user not found
+                if (Objects.isNull(password) || !password.equals(userDetails.getPassword()))
+                    throw new BadCredentialsException("Password in invalid");
+                return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());
+            }
+
+            @Override
+            public boolean supports(Class<?> authentication) {
+                return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+            }
+        };
+    }
+
+    @Bean
+    public UserDetailsService customUserDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user1 = User.withUsername("Swayam")
+                                .password("sid123")
+                                .passwordEncoder(passwordEncoder::encode)
+                                .build();
+
+        UserDetails user2 = User.withUsername("DJ")
+                                .password("dj123")
+                                .passwordEncoder(passwordEncoder::encode)
+                                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
---
+++ spring-web/spring-web-filter-interceptor/src/main/java/io/wulfcodes/filinc/FilincApplication.java
@@ -0,0 +1,13 @@
+package io.wulfcodes.filinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilincApplication.class, args);
+	}
+
+}
